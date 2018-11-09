b17d35e15c46904109e39aaf709c5e04c4ec8b89
Archita Mittal <archita810@gmail.com>
2018-11-09 19:39:00 +0530

    Recieving Successful

--- app/src/main/java/com/example/archi/myapplication/CameraActivity.java
+++ app/src/main/java/com/example/archi/myapplication/CameraActivity.java
@@ -1,11 +1,16 @@
 package com.example.archi.myapplication;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.net.sip.SipSession;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.provider.Telephony;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -19,6 +24,7 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+
 public class CameraActivity extends AppCompatActivity {
     public static final int REQUEST_IMAGE_CAPTURE=1;
     private FirebaseAuth firebaseAuth;
@@ -27,6 +33,9 @@ public class CameraActivity extends AppCompatActivity {
     ImageView imageView;
     SmsManager smsManager;
     TextView display;
+    //SmsVerifyCatcher smsVerifyCatcher;
+    //private SmsBroadcastReceiver smsBroadcastReciever;
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +48,26 @@ public class CameraActivity extends AppCompatActivity {
         send = findViewById(R.id.send);
         smsManager = SmsManager.getDefault();
         display = findViewById(R.id.display);
+        /*SmsReciever.bindListener(new SmsReciever.SmsListener() {
+            @Override
+            public void messageReceived(String messageText) {
+                Log.d("TAG", "messageReceived: ");
+                display.setText(messageText);
+                //Toast.makeText(this,"recieved",Toast.LENGTH_LONG).show();
+            }
+        });*/
+
+
+        /*smsVerifyCatcher = new SmsVerifyCatcher(this,new OnSmsCatchListener<String>() {
+            @Override
+            public void onSmsCatch(String message) {
+                Log.d("listenning", "onSmsCatch: ");
+                display.setText(message);
+            }
+        });*/
+
+        /*smsVerifyCatcher.setPhoneNumberFilter("VM-VAHAAN");
+        smsVerifyCatcher.setFilter("VAHAAN");*/
         Photobtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,21 +82,7 @@ public class CameraActivity extends AppCompatActivity {
                 smsManager.sendTextMessage("7738299899",null,text,null,null);
                 Log.d("Camera", "Sent");
                 //Toast.makeText(this,"sent",Toast.LENGTH_LONG).show();
-                Uri mSmsinboxQueryUri = Uri.parse("content://sms/inbox");
-                String[] projection = {"address", "body"};
-                String phoneNumber = "8582246";
 
-                Cursor cursor1 = getContentResolver().query(mSmsinboxQueryUri,
-                        projection,
-                        "address = ?",
-                        new String[] {phoneNumber},
-                        "date DESC LIMIT 1");
-                String body;
-                if (cursor1 != null && cursor1.moveToFirst()) {
-                    body = cursor1.getString(cursor1.getColumnIndex("body"));
-                    display = (TextView) findViewById(R.id.display);
-                    display.setText(body);
-                }
             }
         });
 
@@ -96,6 +111,15 @@ public class CameraActivity extends AppCompatActivity {
 
             }
         });
+        String msg;
+        SmsReciever.bindListener(new SmsReciever.SmsListener() {
+            @Override
+            public void messageReceived(String messageText) {
+                Toast.makeText(CameraActivity.this, messageText, Toast.LENGTH_SHORT).show();
+                display.setText(messageText);
+
+            }
+        });
 
         Photobtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -107,9 +131,10 @@ public class CameraActivity extends AppCompatActivity {
             }
         });
 
-
-
     }
+
+
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
@@ -118,4 +143,5 @@ public class CameraActivity extends AppCompatActivity {
             imageView.setImageBitmap(imageBitmap);
         }
     }
+
 }
